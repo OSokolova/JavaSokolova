1a575dbb4921050b6ee94533b862f8ff190aca36
OSokolova <tafik@ngs.ru>
2018-02-08 19:31:53 +0700

    добавила файл с описанием класса

---
+++ Range/src/Range.java
@@ -0,0 +1,19 @@
+package ru.academits.sokolova.range
+
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean IsInside(double number) {
+        return number >= this.from && number <= this.to;
+    }
+}
