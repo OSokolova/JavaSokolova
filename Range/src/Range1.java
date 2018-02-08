bf2183dcd14859e1060fbb9a4c8e79a145f0c3aa
OSokolova <tafik@ngs.ru>
2018-02-08 19:31:09 +0700

    добавила файл с описанием класса

--- Range/src/Range1.java
+++ Range/src/Range1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Range1 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите диапазон");
         Range range1 = new Range(scanner.nextDouble(), scanner.nextDouble());
