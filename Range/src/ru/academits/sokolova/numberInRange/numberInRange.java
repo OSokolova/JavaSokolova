a8a290986f8754a4ebb48fb2dc959e4bf734c5e4
OSokolova <tafik@ngs.ru>
2018-02-09 15:30:19 +0700

    поля успешно заполняются в numberInRange

--- Range/src/ru/academits/sokolova/numberInRange/numberInRange.java
+++ Range/src/ru/academits/sokolova/numberInRange/numberInRange.java
@@ -1,4 +1,5 @@
 package ru.academits.sokolova.numberInRange;
+
 import ru.academits.sokolova.range.Range;
 
 import java.util.Scanner;
@@ -6,13 +7,10 @@ import java.util.Scanner;
 public class numberInRange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Range range1 = new Range(from,to);
         System.out.println("Введите диапазон");
-        range1.setFrom(scanner.nextDouble());
-        range1.setTo(scanner.nextDouble());
+        Range range1 = new Range(scanner.nextDouble(), scanner.nextDouble());
         System.out.println("Длина диапазона равна " + range1.getLength());
         System.out.println("Введите число");
-
         if (range1.isInside(scanner.nextDouble())) {
             System.out.println("Число принадлежит диапазону");
         } else {
