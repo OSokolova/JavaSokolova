0a3d2c66db8bb91073aec50572f843de9c6fca56
OSokolova <tafik@ngs.ru>
2018-03-30 00:21:17 +0700

    исправила вторую часть и упростила третью

--- ArrayListHome/src/ru/academits/sokolova/ArrayListHome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/sokolova/ArrayListHome/ArrayListHome.java
@@ -8,8 +8,7 @@ import java.util.Scanner;
 
 public class ArrayListHome {
     public static void main(String... args) throws IOException {
-        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\pingvin\\IdeaProjects\\JavaSokolova\\CSVToHTML\\CSV.csv"))
-        ) {
+        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\pingvin\\IdeaProjects\\JavaSokolova\\CSVToHTML\\CSV.csv"))) {
             ArrayList<String> names = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 String s = scanner.nextLine();
@@ -17,23 +16,23 @@ public class ArrayListHome {
             }
             System.out.println(names);
         }
-        ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 4, 5, 6));
         for (int i = 0; i < numbers1.size(); i++) {
             if (numbers1.get(i) % 2 == 0) {
                 numbers1.remove(i);
+                i--;
             }
         }
         System.out.println(numbers1);
 
-        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
+        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5, 5));
         ArrayList<Integer> numbers3 = new ArrayList<>();
 
-        for (int i = 0; i < numbers2.size(); i++) {
-            for (int j = i + 1; j < numbers2.size(); j++)
-                if (numbers2.get(i).equals(numbers2.get(j))) {
-                    numbers2.remove(j);
-                }
-            numbers3.add(numbers2.get(i));
+        for (Integer e : numbers2) {
+            if (!numbers3.contains(e)) {
+                numbers3.add(e);
+            }
         }
         System.out.println(numbers3);
     }
